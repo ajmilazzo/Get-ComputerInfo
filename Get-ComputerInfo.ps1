1605d9f2d94323486cd7f52eace91386f1f8c4df
Marcin Wisniowski <hollowlink@gmail.com>
2019-05-22 04:43:45 -0400

    Use PowerShell 2.0 compatible cmdlets

--- Get-ComputerInfo.ps1
+++ Get-ComputerInfo.ps1
@@ -1,7 +1,7 @@
 function Get-ComputerInfo {
     [CmdletBinding()]
     param (
-        # Specfies the name of the computer to query for information
+        # Specifies the name of the computer to query for information
         [Parameter(
             Position = 0,
             ValueFromPipeline = $true)]
@@ -9,15 +9,21 @@ function Get-ComputerInfo {
         $ComputerName = $env:COMPUTERNAME
     )
 
+    begin {
+        $driveType = @{
+            2 = "Removable Disk"
+            3 = "Fixed Local Disk"
+            4 = "Network Disk"
+            5 = "Compact Disk"
+        }
+    }
     process {
         foreach ($computer in $ComputerName) {
-            $output = New-Object -TypeName PSObject
-            $disks = @(Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computer |
-                Where-Object { $_.DriveType -in 2..3 })
+            $volumes = @(Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computer)
             $processors = @(Get-WmiObject -Class Win32_Processor -ComputerName $computer)
             $memory = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $computer
 
-            $properties = [System.Collections.Specialized.OrderedDictionary]::new()
+            $properties = New-Object -TypeName System.Collections.Specialized.OrderedDictionary
             $properties.Add("ComputerName", $computer)
 
             for ($i = 0; $i -lt $processors.Count; $i++) {
@@ -25,17 +31,31 @@ function Get-ComputerInfo {
                 $properties.Add("Processor$($i+1)Caption", $processors[$i].Caption)
                 $properties.Add("Processor$($i+1)Manufacturer", $processors[$i].Manufacturer)
                 $properties.Add("Processor$($i+1)MaxClockSpeed", $processors[$i].Maxclockspeed)
+                $properties.Add("Processor$($i+1)NumberOfCores", $processors[$i].NumberOfCores)
+                $properties.Add("Processor$($i+1)NumberOfLogicalProcessors",
+                    $processors[$i].NumberOfLogicalProcessors)
             }
 
-            $properties.Add("Memory(GB)", ([math]::Round(($memory.TotalPhysicalMemory / 1GB), 2)))
+            $properties.Add("TotalCPUs",
+                ($processors | Measure-Object -Property NumberOfLogicalProcessors -Sum).Sum)
+            $properties.Add("TotalMemory(GB)",
+                [math]::Ceiling(($memory.TotalPhysicalMemory / 1GB)))
 
-            for ($i = 0; $i -lt $disks.Count; $i++) {
-                $properties.Add("Volume$($i+1)Letter", $disks[$i].DeviceId)
-                $properties.Add("Volume$($i+1)Size(GB)", ([math]::Round(($disks[$i].Size / 1GB), 2)))
-                $properties.Add("Volume$($i+1)FreeSpace(GB)", ([math]::Round(($disks[$i].FreeSpace / 1GB), 2)))
+            for ($i = 0; $i -lt $volumes.Count; $i++) {
+                $volId = $volumes[$i].DeviceId.Substring(0,1)
+                $properties.Add("Volume$($volId)Size(GB)",
+                    ([math]::Round(($volumes[$i].Size / 1GB), 2)))
+                $properties.Add("Volume$($volId)FreeSpace(GB)",
+                    ([math]::Round(($volumes[$i].FreeSpace / 1GB), 2)))
+                $properties.Add("Volume$($volId)DriveType",
+                    $driveType[[int]$volumes[$i].DriveType])
             }
 
-            $output | Add-Member $properties
+            $output = New-Object -TypeName PSObject
+            $properties.GetEnumerator() | ForEach-Object {
+                $output | Add-Member -MemberType NoteProperty -Name $_.Key -Value $_.Value
+            }
+
             $output
         }
     }
